fed23d5333268d25096e22d33cde4e9ca8052802
Chaemin-Lim <antraxmin@naver.com>
2025-04-18 23:41:05 +0900

    [fix] import 오류 수정 (pydantic > pydantic_settings)

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME")
     
     UPLOAD_DIR: str = os.getenv("UPLOAD_DIR")
-    MAX_UPLOAD_SIZE: int = int(os.getenv("MAX_UPLOAD_SIZE"))  
+    MAX_UPLOAD_SIZE: int = int(os.getenv("MAX_UPLOAD_SIZE", 10 * 1024 * 1024))  
     ALLOWED_EXTENSIONS: list = ["jpg", "jpeg", "png"]
     
     SECRET_KEY: str = os.getenv("SECRET_KEY")
